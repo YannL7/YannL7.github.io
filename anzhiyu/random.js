3354848a5f2d4da0c73c1095cf1b3251fbc69475
YannL7 <xyme000@163.com>
2024-04-13 21:45:36 +0800

    Site updated: 2024-04-13 21:45:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/22/hello-world/"];function toRandomPost(){
+var posts=["2023/10/22/hello-world/","2024/04/13/笨蛋/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
